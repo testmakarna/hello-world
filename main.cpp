ab8b7d7f92457c3c37b209a535e717788231e555
Emre Alyamaç <140554687+testmakarna@users.noreply.github.com>
2025-03-27 11:08:47 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (auto &&i : msg)
+    {
+        std::cout << i << " ";
+    }
+    
+    std::cout << std::endl;
     return 0;
 }
